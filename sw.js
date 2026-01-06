bc9fdf946bf715903c5bcd39188ed8fa286ceb12
lwyaoko <51454417+lwyaoko@users.noreply.github.com>
2026-01-06 21:54:42 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Worker: PWAとして認識させるための最小構成
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // ネットワークから取得するだけの空のフェッチイベント
-  e.respondWith(fetch(e.request));
-});
